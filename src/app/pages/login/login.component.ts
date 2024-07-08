31cf420cf313fd8233f34de6864da1e956c3ca78
charles santos <csbs.conta@outlook.com>
2024-07-08 12:20:41 -0300

    [LanguageListComponent] create

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { AppAbstract } from '../../app.abstract';
 import { KeyboardComponent } from '../../components/keyboard/keyboard.component';
 import { LanguageListComponent } from '../../components/language-list/language-list.component';
 import { LogoComponent } from '../../components/logo/logo.component';
@@ -12,12 +13,13 @@ import { LogoComponent } from '../../components/logo/logo.component';
   imports: [NgIf, LogoComponent, KeyboardComponent, TranslateModule, RouterModule, LanguageListComponent],
   templateUrl: './login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent extends AppAbstract {
   protected readonly minStageLogin: number = 0;
   protected readonly maxStageLogin: number = 2;
 
+  protected btnText: string = 'CONFIRM';
   protected text: string = '';
-  protected stageLogin: number = 1;
+  protected stageLogin: number = 0;
 
   protected listLandingTexts: string[] = [
     'CANCEL_WHENEVER', 'UNLIMITED_CONTENT', 'WATCH_WHENEVER', 'WATCH_PAUSE', 'AFFORDABLE_PRICE'
@@ -27,13 +29,8 @@ export class LoginComponent {
     this.text = event;
   }
 
-  protected nextStageLogin(): void {
+  protected confirmLogin(): void {
     if (this.stageLogin < this.maxStageLogin) this.stageLogin++;
-    console.log(this.stageLogin);
-  }
-
-  protected previousStageLogin(): void {
-    if (this.stageLogin > this.minStageLogin) this.stageLogin--;
     console.log(this.stageLogin);
   }
 
